48b2fe021fd637ea16ed668a44c1f12ac63773c9
masf <marceloandradesilvafilho@gmail.com>
2022-08-07 12:35:05 -0300

    Adição de classe suíte para selecionar classes de testes executadas

--- src/test/java/br/ce/wcaquino/suites/SuiteExecucao.java
+++ src/test/java/br/ce/wcaquino/suites/SuiteExecucao.java
@@ -1,4 +1,16 @@
 package br.ce.wcaquino.suites;
 
+import br.ce.wcaquino.servicos.CalculoValorLocacao;
+import br.ce.wcaquino.servicos.LocacaoServiceTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        CalculoValorLocacao.class,
+        LocacaoServiceTest.class
+})
+
 public class SuiteExecucao {
+    // Remover se puder
 }
